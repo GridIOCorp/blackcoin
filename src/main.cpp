d4a35171b8a10070873c66f0ee15922f95251412
Pavel Vasin <pavel@vasin.nl>
2014-12-13 14:17:15 +0300

    accept blocks with unknown version

--- src/main.cpp
+++ src/main.cpp
@@ -1994,8 +1994,10 @@ bool CBlock::AcceptBlock()
 {
     AssertLockHeld(cs_main);
 
-    if (nVersion > CURRENT_VERSION)
-        return DoS(100, error("AcceptBlock() : reject unknown block version %d", nVersion));
+    if (!IsProtocolV3()) {
+        if (nVersion > CURRENT_VERSION)
+            return DoS(100, error("AcceptBlock() : reject unknown block version %d", nVersion));
+    }
 
     // Check for duplicate
     uint256 hash = GetHash();
